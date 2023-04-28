62be1c8966e8f081c159e13208fdcccc4ce0258b
bishalprasad321 <bishalprasad321@gmail.com>
2023-04-28 22:09:42 +0530

    Implemented empty screen message for unavailability of server or internet

--- app/src/main/java/com/bishal/narutoapp/presentation/common/ListContent.kt
+++ app/src/main/java/com/bishal/narutoapp/presentation/common/ListContent.kt
@@ -93,6 +93,7 @@ fun handlePagingResult(
                 false
             }
             error != null -> {
+                EmptyScreen(error = error)
                 false
             }
             else -> true
